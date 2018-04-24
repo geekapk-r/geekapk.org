da4d17c6d0d24e4cdae0854c249a249de5d23f72
duangsuse <fedora-opensuse@outlook.com>
2018-04-24 11:38:54 +0800

    Add switch for geekstyle shouldApplyDarkStyle

--- js/geekstyle.js
+++ js/geekstyle.js
@@ -4,19 +4,23 @@
 
 // should use dark style from 19:00 to 6:00
 function shouldApplyDarkStyle() {
-  if (localStorage['night'] == false.toString())
-    return false;
+  var conf = localStorage['night']
+  if (conf != null) {
+    if (conf == false.toString())
+      return false;
+    else return true;
+  }
   var dh = new Date();
-  nightBegin = localStorage['nightBegin'] || 19
-  nightEnd = localStorage['nightEnd'] || 6
+  var nightBegin = localStorage['nightBegin'] || 19
+  var nightEnd = localStorage['nightEnd'] || 6
   return dh.getHours() >= nightBegin && dh.getHours() <= nightEnd
 }
 
 // call this to apply dark style automatically
 function autodark() {
   if (shouldApplyDarkStyle()) {
-    body = $$('body')[0]
-    cls = body.classList
-    cls.add('mdui-theme-layout-dark')
+    var body = $$('body')[0]
+    var cls = body.classList
+    var cls.add('mdui-theme-layout-dark')
   }
 }
